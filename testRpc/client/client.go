d369cd1ff7e02b66cd0c4fddc8adc8f017846083
Emmanuel Oppong <eoppong@gsumail.gram.edu>
2026-01-12 02:47:48 -0600

    test: done practing rpc

--- testRpc/client/client.go
+++ testRpc/client/client.go
@@ -6,3 +6,26 @@ import "fmt"
 type Params struct {
 	A, B int64
 }
+
+func main() {
+	client, err := rpc.Dial("tcp", "localhost:8080")
+
+	if err != nil {
+		fmt.Println("Could not connect to rpc")
+		return 
+	}
+
+	defer client.Close()
+
+	params := Params{A: 2, B: 4}
+	var result int64
+
+	err = client.Call("Calculater.Add", &params, &result)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Result:", result)
+}
