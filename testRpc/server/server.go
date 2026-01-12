da5d19dbc48651de2524c29d227ffccf7c0f4e1f
Emmanuel Oppong <eoppong@gsumail.gram.edu>
2026-01-12 03:04:09 -0600

    test: done practing rpc

--- testRpc/server/server.go
+++ testRpc/server/server.go
@@ -20,3 +20,33 @@ func (Calc *Calculater) Sub(param *Params, result *int64) error {
 	return nil
 }
 
+func (Calc *Calculater) Divide(param *Params, result *int64) error {
+	res := param.A / param.B
+
+	if res == 0 {
+		return fmt.Errorf("ZeroDivisionError")
+	}
+
+	*result = res
+	return nil
+}
+
+func (Calc *Calculater) Multiply(param *Params, result *int64) error {
+	*result = param.A * param.B
+	return nil
+}
+
+func main() {
+	calc := new(Calculater)
+	rpc.Register(calc)
+
+	listener, err := net.Listen("tcp", ":8080")
+    if err != nil {
+        fmt.Println("Error listening:", err)
+        return
+    }
+    defer listener.Close()
+
+    fmt.Println("Server is listening on port 8080...")
+    rpc.Accept(listener)
+}
